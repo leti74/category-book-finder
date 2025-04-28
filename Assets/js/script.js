683628302288b095f9f963b874de730ace6a0819
leti74 <letiimar4@gmail.com>
2025-04-28 18:13:55 +0200

    oop

---
+++ Assets/js/script.js
@@ -0,0 +1,95 @@
+class Libro {
+  constructor(
+    title,
+    keyBooks,
+    authors,
+    descriptionBook = "Descrizione non disponibile"
+  ) {
+    this.title = title;
+    this.keyBooks = keyBooks;
+    this.authors = authors;
+    this.descriptionBook = descriptionBook;
+  }
+
+  async fetchDescription() {
+    try {
+      let response = await fetch(
+        `https://openlibrary.org${this.keyBooks}.json`
+      );
+      let data = await response.json();
+      this.descriptionBook = data.description
+        ? typeof data.description === "string"
+          ? data.description
+          : data.description.value
+        : "Descrizione non disponibile";
+    } catch (error) {
+      console.error("Errore nel recupero della descrizione:", error);
+      this.descriptionBook = "Errore nel recupero della descrizione.";
+    }
+  }
+
+  render() {
+    let resultItem = document.createElement("div");
+    resultItem.classList.add("result-item");
+    resultItem.innerHTML = `
+        <div class="info-book">
+          <h2 class="js-title">${this.title}</h2>
+          <p>${this.authors}</p>
+        </div>
+      `;
+
+    let titleBook = resultItem.querySelector(".js-title");
+    titleBook.addEventListener("click", async () => {
+      await this.fetchDescription();
+      let resultDescription = resultItem.querySelector(".js-description");
+      if (!resultDescription) {
+        resultDescription = document.createElement("div");
+        resultDescription.classList.add("js-description");
+        resultDescription.innerHTML = `<p>${this.descriptionBook}</p>`;
+        resultItem.appendChild(resultDescription);
+      }
+    });
+
+    return resultItem;
+  }
+}
+
+const input = document.querySelector(".js-input");
+const button = document.querySelector(".js-button");
+const result = document.querySelector(".js-result");
+
+button.addEventListener("click", async () => {
+  let categoria = input.value.trim();
+  result.innerHTML = "";
+
+  if (!categoria || categoria === undefined) {
+    result.innerHTML = "<p>Inserisci una categoria valida.</p>";
+    return;
+  }
+
+  try {
+    let response = await fetch(
+      `https://openlibrary.org/subjects/${categoria}.json`
+    );
+    let data = await response.json();
+
+    data.works.forEach((work) => {
+      let libro = new Libro(
+        work.title,
+        work.key,
+        work.authors.map((author) => author.name).join(", ")
+      );
+
+      result.appendChild(libro.render());
+    });
+  } catch (error) {
+    console.error("Errore:", error);
+    result.innerHTML = "<p>Si è verificato un errore.</p>";
+  }
+});
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    button.click();
+  }
+});
